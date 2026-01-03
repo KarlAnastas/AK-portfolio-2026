7d210fe6d9ce3c598782fe3cab724b8b4d98e3d5
Anastasia Karlova <anastasiakarlova@MacBook-Air-2.local>
2026-01-04 01:50:31 +0900

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/AK-portfolio-2026/", 
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
